8ccd12de2de0c394fe988b597d80c634436b4626
Neha <neha19031903@gmail.com>
2026-09-22 00:01:20 +0530

    Initial commit

---
+++ home.js
@@ -0,0 +1,116 @@
+import express from 'express'
+import path from 'path'
+import { MongoClient, ObjectId } from 'mongodb'
+const app=express()
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:true}))
+const publicpath=path.resolve('public')
+app.use(express.static(publicpath))
+
+// connectiong to db
+
+const dbname='node-project'
+const collection='todo'
+const url='mongodb://localhost:27017'
+const client= new MongoClient(url)
+
+const dbconnection=async ()=>{
+    const connect=await client.connect()
+    return await connect.db(dbname)
+}
+
+app.get('', (req, resp)=>{
+    resp.render('home')
+    
+})
+
+app.get('/tasklist', async(req, resp)=>{
+    const db=await dbconnection()
+    const collectionname=db.collection(collection)
+    const result=await collectionname.find().toArray()
+    resp.render('tasks', {result})
+})
+
+app.get('/add', (req, resp)=>{
+    resp.render('add')
+})
+
+app.get('/update', (req, resp)=>{
+    resp.render('update')
+})
+
+app.post('/submit', async(req, resp)=>{
+    const db=await dbconnection()
+    const collectionname=db.collection(collection)
+    const result= collectionname.insertOne(req.body)
+    if(result){
+        resp.redirect('/');
+    }
+    else{
+        resp.redirect('/add')
+    }
+    
+})
+app.get('/delete/:id', async(req, resp)=>{
+    const id=req.params.id;
+    const db=await dbconnection()
+    const collectionname=db.collection(collection)
+    const result=collectionname.deleteOne({_id: new ObjectId(id)})
+    if(result){
+        resp.redirect('/')
+    }
+    else{
+        resp.redirect('/tasklist')
+    }
+})
+
+app.get('/update/:id', async(req, resp)=>{
+    const id=req.params.id;
+    const db=await dbconnection()
+    const collectionname=db.collection(collection)
+    const result=await collectionname.findOne({_id: new ObjectId(id)})
+    if(result){
+        resp.render('update', {result})
+    }
+    else{
+        resp.redirect('/tasklist')
+    }
+})
+app.post('/update', async(req,resp)=>{
+    const id=req.body.id
+    const db=await dbconnection()
+    const collectionname=db.collection(collection)
+    const result=await collectionname.updateOne({_id: new ObjectId(id)}, { $set: { title: req.body.title, description: req.body.description}})
+    console.log(result)
+    if(result){
+        resp.redirect('/tasklist')
+    }
+    else{
+        resp.redirect('/update')
+    }
+})
+
+app.post('/multipledelete', async(req, resp)=>{
+    const ids=req.body.selectedcheckbox
+    const db=await dbconnection()
+    const collectionname=db.collection(collection)
+
+    let allobjectids=undefined
+    if(Array.isArray(ids)){
+        allobjectids=req.body.selectedcheckbox.map((id)=>new ObjectId(id))
+    }
+    else{
+        allobjectids=[new ObjectId(req.body.selectedcheckbox)]
+    }
+    
+    const result=await collectionname.deleteMany({_id: { $in: allobjectids}})
+    if(result){
+        resp.redirect('/')
+    }
+    else{
+        resp.redirect('/tasklist')
+    }
+})
+
+app.listen(3000)
